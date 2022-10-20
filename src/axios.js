1117e14d633b76961e9cd2699dae8c1d668d3153
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-10-19 21:11:46 -0300

    add .env file

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+// export default axios.create({
+//     baseURL: process.env.REACT_APP_API_URL_DEV
+// })
+
 export default axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: process.env.REACT_APP_API_URL_PROD
 })
+
